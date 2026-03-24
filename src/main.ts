2753c9c140432bbac3d6bd0248dbb7c572c376cc
Sebastian Agudelo Marin <sebascarman@gmail.com>
2026-03-24 15:55:55 -0500

    feat: initial project

---
+++ src/main.ts
@@ -0,0 +1,32 @@
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: 'https://studio.apollographql.com',
+    credentials: true,
+  });
+
+  // Permitir acceso desde redes públicas a localhost (Private Network Access)
+  app.use((req, res, next) => {
+    if (req.headers['access-control-request-private-network']) {
+      res.setHeader('Access-Control-Allow-Private-Network', 'true');
+    }
+    next();
+  });
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true, 
+    })
+  );
+
+  await app.listen(3000);
+  
+  console.log('Aplicación corriendo en puerto: ', 3000 );
+}
+bootstrap();
